8bf8e689360cab4ec534aed141cb4c208b207b6d
Dunn <dunnjose@USWSCODUNNJOSE1.local>
2014-10-26 14:06:19 -0400

    added a test class

---
+++ JDTest.h
@@ -0,0 +1,13 @@
+//
+//  JDTest.h
+//  Git Test
+//
+//  Created by Dunn, Joseph on 10/26/14.
+//  Copyright (c) 2014 com.merck. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JDTest : NSObject
+
+@end
